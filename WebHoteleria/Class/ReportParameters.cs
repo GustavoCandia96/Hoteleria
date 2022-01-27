1210e91c2a654cd7fc9a085370b050c7f71b5921
GustavoCandia96 <gutycandiama@gmail.com>
2022-01-27 08:32:20 -0300

    Carpeta Class agregado con todos los archivos listos

---
+++ WebHoteleria/Class/ReportParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHoteleria.Class
+{
+    public class ReportParameters
+    {
+
+        public static ReportesParametros DatosReporte
+        {
+            get
+            {
+                return (ReportesParametros)HttpContext.Current.Session["ReportParams"];
+            }
+
+            set
+            {
+                HttpContext.Current.Session["ReportParams"] = value;
+            }
+        }
+
+    }
+
+    public class ReportesParametros
+    {
+
+        #region Propiedades
+
+        public object ReportSource { set; get; }
+        public string ReportPath { set; get; }
+        public string NombreArchivo { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ WebHoteleria/Class/VerificaSesion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebHoteleria.Controllers;
+
+namespace WebHoteleria.Class
+{
+    public class VerificaSesion: ActionFilterAttribute
+    {
+
+        #region Propiedades
+
+        private UsuarioLogin usuario;
+
+        #endregion
+
+        #region Metodos
+
+        /*
+         * METODO QUE VERIFICA SI EL USUARIO HA INICIADO SESION O NO
+         */
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+                usuario = (UsuarioLogin)HttpContext.Current.Session["user"];
+                if (usuario == null)
+                {
+                    if (filterContext.Controller is LoginController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Login/Index");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Login/Index");
+            }
+
+        }
+
+        #endregion
+
+    }
+}
