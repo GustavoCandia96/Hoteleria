6504b2dbf416395f5f2b2e9bee7cf8c55b67f307
GustavoCandia96 <gutycandiama@gmail.com>
2022-02-23 22:40:06 -0300

    Proyecto hasta el modulo sucursales

---
+++ WebHoteleria/Models/PaisModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebHoteleria.Models
+{
+    public class PaisModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Tiene que ingresar entre {2} a {1} caracteres en el campo")]
+        public string NombrePais { get; set; }
+        public bool? Estado { get; set; }
+
+        public string EstadoDescrip { get; set; }
+
+    }
+}
